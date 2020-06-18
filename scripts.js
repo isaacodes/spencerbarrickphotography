eece4d38647162cbf8adba3b73116b4c1040e63f
isaacmaddox <54295143+isaacodes@users.noreply.github.com>
2020-06-18 10:00:46 -0700

    uploaded files
    yeah

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var slide = 1;
+function slideshow() {
+    slide++;
+    if (slide == 6) {
+        slide = 1;
+    }
+    var imgname = "ex_" + slide.toString();
+    $(".pix").css('background-image', 'url("images/' + imgname + '.jpg")');
+}
+setInterval(slideshow, 4000);
+
+var navopen = false;
+$(document).on('click', '.open-links', function() {
+    if (navopen === false) {
+        $(".nav-links").css('left', '0px');
+        navopen = true;
+    } else {
+        $(".nav-links").css('left', '-60vw');
+        navopen = false;
+    }
+});
