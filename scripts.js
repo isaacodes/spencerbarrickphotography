8ca822a6a8c0b93e867e4a459f4da3226b1084ea
isaacmaddox <54295143+isaacodes@users.noreply.github.com>
2020-06-19 08:05:52 -0700

    6.19.20 11:05 AM
    Added filters for gallery and some tweaks

--- scripts.js
+++ scripts.js
@@ -21,8 +21,50 @@ $(document).on('click', '.open-links', function() {
 });
 
 $(document).ready(function() {
+
     $("body").scroll(function() {
         var st = $("body").scrollTop() / 3;
-        $("header").css('background-position-y', 'calc(-50vh + ' + st + 'px)');
+        $("header").css('background-position-y', st + 'px');
+    });
+    $(document).on('click', '.filter', function() {
+        $(".filter").removeClass('selected');
+        $(this).addClass('selected');
+        if ($(this).html() == "Clear Filters") {
+            noFilter();
+            $(this).removeClass('selected');
+        } else {
+            var tag = $(this).html();
+            filterImages(tag);
+        }
+    });
+    $(document).on('click', '.pix', function() {
+        window.location = "gallery.html#carousel";
+    });
+    if(window.location.hash) {
+        filterImages('carousel');
+    }
+
+    $(document).on('mouseover', '.image-container img', function() {
+        var h = $(this).height();
+        var w = $(this).width();
+        var x = $(this).offset().left;
+        var y = $(this).offset().top;
+        $(".overlay").css('display', 'block').css('top', y + 'px').css('left', x + 'px').css('height', h).css('width', w);
+    }).on('mouseleave', '.image-container img', function() {
+        $(".overlay").css('display', 'none');
     });
 });
+
+function filterImages(filterValue) {
+    filterValue = filterValue.toLowerCase();
+    $(".image-container img").hide();
+    $(".image-container img").each(function() {
+        if ($(this).hasClass(filterValue)) {
+            $(this).show();
+        }
+    });
+}
+
+function noFilter() {
+    $(".image-container img").show();
+}
